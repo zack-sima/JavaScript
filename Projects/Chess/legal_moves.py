8584a84a58af83b5c2a21fb7f3f99551fee6211e
floatingtrees <96200235+floatingtrees@users.noreply.github.com>
2023-04-21 19:54:25 -0700

    Update legal_moves.py

--- Projects/Chess/legal_moves.py
+++ Projects/Chess/legal_moves.py
@@ -63,7 +63,6 @@ def pins_and_checks_search(position, position_swapped, white):
 		if king_location_y + i > 7:
 			break
 		search_square = (king_location_y + i, king_location_x) # row_search
-		print(search_square)
 		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
 																		in_check, possibly_pinned_pieces, True, pinned)
 		if pinned:
@@ -95,7 +94,7 @@ def pins_and_checks_search(position, position_swapped, white):
 			break
 		search_square = (king_location_y + 1, king_location_x + 1) # diagonal
 		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
-																		in_check, possibly_pinned_pieces, True, pinned)
+																		in_check, possibly_pinned_pieces, False, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
 			pinned = False
@@ -138,7 +137,7 @@ def pins_and_checks_search(position, position_swapped, white):
 			break
 		search_square = (king_location_y - i, king_location_x - i) # row_search
 		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
-																		in_check, possibly_pinned_pieces, True, pinned)
+																		in_check, possibly_pinned_pieces, False, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
 			pinned = False
@@ -152,7 +151,7 @@ def pins_and_checks_search(position, position_swapped, white):
 			break
 		search_square = (king_location_y + i, king_location_x - i) # row_search
 		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
-																		in_check, possibly_pinned_pieces, True, pinned)
+																		in_check, possibly_pinned_pieces, False, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
 			pinned = False
@@ -162,22 +161,17 @@ def pins_and_checks_search(position, position_swapped, white):
 	possibly_pinned_pieces = []
 
 
-	print()
 	for i in range(1, 7): # - +
 		if king_location_y - i < 0 or king_location_x + i > 7:
 			break
 		search_square = (king_location_y - i, king_location_x + i) # row_search
 		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
-																		in_check, possibly_pinned_pieces, True, pinned)
-		print(in_check)
+																		in_check, possibly_pinned_pieces, False, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
-			print("detected")
 			pinned = False
 			break
 		if stop_searching_row:
-			print("here", king_location_y - i, king_location_x + i)
-			print(possibly_pinned_pieces, position[possibly_pinned_pieces[0]])
 			break
 	possibly_pinned_pieces = []
 
@@ -203,10 +197,11 @@ def legal_move_search(position, position_swapped, white):
 if __name__ == "__main__":
 	white = True
 	# goes row (8-1), column (a-h) # q0 is pinning pawn to king, "q0":(4, 7)
-	start_position = {"r0" : (0, 0), "n0" : (0, 1), "b0" : (0, 2), "q0":(4, 7), "k0" : (0, 4), "b1" : (0, 5), "n2": (0, 6), "r2":(0, 7), 
+	start_position = {"r0" : (0, 0), "n0" : (0, 1), "b0" : (0, 2), "q0":(0, 3), "k0" : (0, 4), "b1" : (0, 5), "n2": (0, 6), "r2":(0, 7), 
 			"p0": (1, 0), "p1": (1, 1), "p2":(1, 2), "p3":(1, 3), "p4":(1, 4), "p5":(1, 5), "p6": (1, 6), "p7":(1, 7),
 			"P0":(6, 0), "P1":(6, 1), "P2":(6, 2), "P3":(6, 3), "P4":(6, 4), "P5":(6, 5), "P6":(6, 6), "P7":(6, 7),
 			"R0":(7, 0), "N0":(7, 1), "B0":(7, 2), "Q0":(7, 3), "K0":(7, 4), "B1":(7, 5), "N1":(7, 6), "R1":(7, 7)}
 	position_swapped = dict([(value, key) for key, value in start_position.items()])
 
 	legal_move_search(start_position, position_swapped, white)
+
