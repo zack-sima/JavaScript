e50974d8866050406d85cbe037e79bfe47d38878
floatingtrees <96200235+floatingtrees@users.noreply.github.com>
2023-04-21 19:45:37 -0700

    Update legal_moves.py
    Actually works now (not tested very well though)

--- Projects/Chess/legal_moves.py
+++ Projects/Chess/legal_moves.py
@@ -1,6 +1,5 @@
-def in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, row):
-	pinned = False
-	if row and white:
+def in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, row, pinned):
+	if row and white: # no issues with this bloc
 		piece_check = "r"
 		piece_safe = 'b'
 	elif row and not white:
@@ -13,25 +12,31 @@ def in_check_from_square(position_swapped, search_square, white, in_check, possi
 		piece_check = "b"
 		piece_safe = 'r'
 
+
 	stop_searching_row = False 
 	try:
 		piece = position_swapped[search_square]
 	except KeyError:
-		print("empty square")
+		pass
 	else:
 		if white:
 			if piece[0] == "p" or piece[0]  == "n" or piece[0] == piece_safe:
 				stop_searching_row = True
-			if piece[0]  == piece_check or piece[0] == "q":
-				in_check.append(search_square)
+			elif piece[0]  == piece_check or piece[0] == "q":
+				if len(possibly_pinned_pieces) == 0:
+					in_check.append(search_square)
+					stop_searching_row = True
+				else:
+					pinned = True
 			else:
 				possibly_pinned_pieces.append(piece)
 		else:
 			if piece[0] == "P" or piece[0] == "N" or piece[0]  == piece_safe:
 				stop_searching_row = True
 			elif piece[0] == piece_check or piece[0] == "Q":
-				if len(possibly_pinned_pieces) != 0:
+				if len(possibly_pinned_pieces) == 0:
 					in_check.append(search_square)
+					stop_searching_row = True
 				else:
 					pinned = True
 			else:
@@ -43,97 +48,150 @@ def in_check_from_square(position_swapped, search_square, white, in_check, possi
 
 
 def pins_and_checks_search(position, position_swapped, white):
+	pinned =  False
 	in_check = []
 	pinned_pieces = []
 	possibly_pinned_pieces = []
 
 	if white:
-		king_location_x, king_location_y = position["K0"]
+		king_location_y, king_location_x = position["K0"]
 	else:
-		king_location_x, king_location_y = position["k0"]
+		king_location_y, king_location_x = position["k0"]
+
+
 	for i in range(1, 7): # + 0
-		if king_location_x + i > 7:
-			break
-		search_square = (king_location_x + 1, king_location_y) # row_search
-		in_check, stop_searching_row, possibly_pinned_pieces, pinned  = in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, True)
-		if pinned:
-			pinned_pieces.append((possibly_pinned_pieces, search_square))
-			break
-		if stop_searching_row or in_check:
-			break
-	for i in range(1, 7): # 0 +
 		if king_location_y + i > 7:
 			break
-		search_square = (king_location_x, king_location_y + 1) # row_search
-		in_check, stop_searching_row, possibly_pinned_pieces, pinned  = in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, True)
+		search_square = (king_location_y + i, king_location_x) # row_search
+		print(search_square)
+		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
+																		in_check, possibly_pinned_pieces, True, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
+			pinned = False
 			break
-		if stop_searching_row or in_check:
+		if stop_searching_row:
 			break
+	possibly_pinned_pieces = []
+
+
+	for i in range(1, 7): # 0 +
+		if king_location_x + i > 7:
+			break
+		search_square = (king_location_y, king_location_x + i) # row_search
+		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
+																		in_check, possibly_pinned_pieces, True, pinned)
+		if pinned:
+			pinned_pieces.append((possibly_pinned_pieces, search_square))
+			pinned = False
+			break
+		if stop_searching_row:
+			break
+
+
+	possibly_pinned_pieces = []
 	for i in range(1, 7): # + + 
-		if king_location_x + i > 7 or king_location_y + i > 7:
+		if king_location_y + i > 7 or king_location_x + i > 7:
 			break
-		search_square = (king_location_x + 1, king_location_y + 1) # diagonal
-		in_check, stop_searching_row, possibly_pinned_pieces, pinned  = in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, False)
+		search_square = (king_location_y + 1, king_location_x + 1) # diagonal
+		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
+																		in_check, possibly_pinned_pieces, True, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
+			pinned = False
 			break
-		if stop_searching_row or in_check:
+		if stop_searching_row:
 			break
+	possibly_pinned_pieces = []
+
+
 	for i in range(1, 7): # - 0
-		if king_location_x - i < 0:
-			break
-		search_square = (king_location_x - 1, king_location_y) # row_search
-		in_check, stop_searching_row, possibly_pinned_pieces, pinned  = in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, True)
-		if pinned:
-			pinned_pieces.append((possibly_pinned_pieces, search_square))
-			break
-		if stop_searching_row or in_check:
-			break
-	for i in range(1, 7): # 0 -
 		if king_location_y - i < 0:
 			break
-		search_square = (king_location_x, king_location_y - 1) # row_search
-		in_check, stop_searching_row, possibly_pinned_pieces, pinned  = in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, True)
+		search_square = (king_location_y - i, king_location_x) # row_search
+		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
+																		in_check, possibly_pinned_pieces, True, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
+			pinned = False
 			break
-		if stop_searching_row or in_check:
+		if stop_searching_row:
 			break
+	possibly_pinned_pieces = []
+
+	for i in range(1, 7): # 0 -, Problem here
+		if king_location_x - i < 0:
+			break
+		search_square = (king_location_y, king_location_x - i) # row_search
+		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
+																		in_check, possibly_pinned_pieces, True, pinned)
+		if pinned:
+			pinned_pieces.append((possibly_pinned_pieces, search_square))
+			pinned = False
+			break
+		if stop_searching_row:
+			break
+	possibly_pinned_pieces = []
+
 	for i in range(1, 7): # - -
-		if king_location_x - i < 0 or king_location_y - i < 0:
+		if king_location_y - i < 0 or king_location_x - i < 0:
 			break
-		search_square = (king_location_x - 1, king_location_y - 1) # row_search
-		in_check, stop_searching_row, possibly_pinned_pieces, pinned  = in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, True)
+		search_square = (king_location_y - i, king_location_x - i) # row_search
+		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
+																		in_check, possibly_pinned_pieces, True, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
+			pinned = False
 			break
-		if stop_searching_row or in_check:
+		if stop_searching_row:
 			break
+	possibly_pinned_pieces = []
+
 	for i in range(1, 7): # + -
-		if king_location_x + i > 7 or king_location_y - i < 0:
+		if king_location_y + i > 7 or king_location_x - i < 0:
 			break
-		search_square = (king_location_x + 1, king_location_y - 1) # row_search
-		in_check, stop_searching_row, possibly_pinned_pieces, pinned  = in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, True)
+		search_square = (king_location_y + i, king_location_x - i) # row_search
+		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
+																		in_check, possibly_pinned_pieces, True, pinned)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
+			pinned = False
 			break
-		if stop_searching_row or in_check:
+		if stop_searching_row:
 			break
+	possibly_pinned_pieces = []
+
+
+	print()
 	for i in range(1, 7): # - +
-		if king_location_x - i < 0 or king_location_y + i < 7:
+		if king_location_y - i < 0 or king_location_x + i > 7:
 			break
-		search_square = (king_location_x - 1, king_location_y + 1) # row_search
-		in_check, stop_searching_row, possibly_pinned_pieces, pinned  = in_check_from_square(position_swapped, search_square, white, in_check, possibly_pinned_pieces, True)
+		search_square = (king_location_y - i, king_location_x + i) # row_search
+		in_check, stop_searching_row, possibly_pinned_pieces, pinned = in_check_from_square(position_swapped, search_square, white, 
+																		in_check, possibly_pinned_pieces, True, pinned)
+		print(in_check)
 		if pinned:
 			pinned_pieces.append((possibly_pinned_pieces, search_square))
+			print("detected")
+			pinned = False
 			break
-		if stop_searching_row or in_check:
+		if stop_searching_row:
+			print("here", king_location_y - i, king_location_x + i)
+			print(possibly_pinned_pieces, position[possibly_pinned_pieces[0]])
 			break
-	print(pinned_pieces, in_check)
+	possibly_pinned_pieces = []
+
+
+	print("pins: ", pinned_pieces, "checks: ", in_check)
 	return pinned_pieces, in_check
 			
+
+
+
+	
+
+
+
 
 
 
@@ -142,15 +200,13 @@ def legal_move_search(position, position_swapped, white):
 
 
 
-
 if __name__ == "__main__":
 	white = True
-	# goes row (8-1), column (a-h)
-	start_position = {"r0" : (0, 0), "n0" : (0, 1), "b0" : (0, 2), "q0":(0, 3), "k0" : (0, 4), "b1" : (0, 5), "n2": (0, 6), "r2":(0, 7), 
+	# goes row (8-1), column (a-h) # q0 is pinning pawn to king, "q0":(4, 7)
+	start_position = {"r0" : (0, 0), "n0" : (0, 1), "b0" : (0, 2), "q0":(4, 7), "k0" : (0, 4), "b1" : (0, 5), "n2": (0, 6), "r2":(0, 7), 
 			"p0": (1, 0), "p1": (1, 1), "p2":(1, 2), "p3":(1, 3), "p4":(1, 4), "p5":(1, 5), "p6": (1, 6), "p7":(1, 7),
 			"P0":(6, 0), "P1":(6, 1), "P2":(6, 2), "P3":(6, 3), "P4":(6, 4), "P5":(6, 5), "P6":(6, 6), "P7":(6, 7),
-			"R0":(7, 0), "N0":(7, 1), "B0":(7, 2), "Q0":(7, 3), "K0":(7, 4), "B1":(0, 5), "N1":(0, 6), "R1":(0, 7)}
+			"R0":(7, 0), "N0":(7, 1), "B0":(7, 2), "Q0":(7, 3), "K0":(7, 4), "B1":(7, 5), "N1":(7, 6), "R1":(7, 7)}
 	position_swapped = dict([(value, key) for key, value in start_position.items()])
 
 	legal_move_search(start_position, position_swapped, white)
-
